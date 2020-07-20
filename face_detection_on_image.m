01ad9c63333701dd07948e24f639ccb9a31c3439
yakshasj <68420311+yakshasj@users.noreply.github.com>
2020-07-20 22:08:41 +0530

    Add files via upload

---
+++ face_detection_on_image.m
@@ -0,0 +1,22 @@
+clc; clear all
+
+the_image= imread('face.jpg');
+
+[width,height]= size(the_image);
+
+
+if width > 320
+    
+   the_image= imresize(the_image, [320 , NaN]);
+end
+
+face_detector = vision.CascadeObjectDetector() ;
+
+
+location_of_the_face = step(face_detector , the_image);
+
+detected_image= insertShape(the_image, 'rectangle', location_of_the_face);
+
+figure ;
+
+imshow(detected_image);
